711b26b1cb25462b648702778a888f65746f870d
Marcos Rene <renemoura789@gmail.com>
2024-04-25 14:45:59 -0300

    feat: add route to getProfile method

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -12,6 +12,7 @@ import {
 
 import { createAccount } from './routes/auth/createAccount'
 import { authenticateWithPassword } from './routes/auth/authenticateWithPassword'
+import { getProfile } from './routes/auth/getProfile'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -42,6 +43,7 @@ app.register(fastifyCors)
 
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(getProfile)
 
 app
   .listen({
