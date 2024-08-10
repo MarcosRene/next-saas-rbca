e0ffbb53df79216c992f926a15996e35364839a3
Marcos Rene <renemoura789@gmail.com>
2024-08-10 19:52:44 -0300

    feat: add create project form

--- apps/web/src/app/auth/sign-up/sign-up-form.tsx
+++ apps/web/src/app/auth/sign-up/sign-up-form.tsx
@@ -11,7 +11,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
-import { useFormState } from '@/hooks/useFormState'
+import { useFormState } from '@/hooks/use-form-state'
 
 import { signInWithGithub } from '../actions'
 import { signUpAction } from './actions'
